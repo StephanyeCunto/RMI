d1d416bf0bc5698c0df7cb441d3f82e7a7d8bc60
StephanyeCunto <stephanyecristine6@gmail.com>
2025-10-13 11:42:25 -0300

    Possibilidade de mudar vídeo

--- src/main/java/com/controller/CreateHost.java
+++ src/main/java/com/controller/CreateHost.java
@@ -3,18 +3,29 @@ package com.controller;
 import com.service.*;
 import com.model.Host;
 
-import javafx.fxml.FXML;
+import javafx.fxml.*;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.media.*;
 
 import java.io.File;
 import java.rmi.registry.LocateRegistry;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class CreateHost {
+
+public class CreateHost implements Initializable{
     @FXML private TextField hostField, serviceField, portField;
     @FXML private VBox vboxContainer;
     @FXML private MediaView mediaView;
+    @FXML private ComboBox<String> videoComboBox;
+    String videoFileAdress = "src/main/resources/video.mp4";
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadComboBox();
+    }
 
     @FXML
     public void createHost(){
@@ -27,6 +38,24 @@ public class CreateHost {
         } catch (Exception e) {
             System.err.println("Erro:"+ "Erro ao criar host: " + e.getMessage());
         }
+    }
+
+    private void loadComboBox(){  
+        File folder = new File("src/main/resources");
+        File[] files = folder.listFiles();
+        if (files == null){
+            videoComboBox.setDisable(true);
+            return;
+        }
+
+        for (File file : files) videoComboBox.getItems().add(file.getName());
+
+        if (!videoComboBox.getItems().isEmpty()) {
+            videoComboBox.getSelectionModel().selectFirst();
+            videoFileAdress = folder + "/" + videoComboBox.getValue();
+        } 
+
+        videoComboBox.valueProperty().addListener((obs, oldVal, newVal) -> videoFileAdress = folder + "/" + videoComboBox.getValue());
     }
 
     private void initHost(Host host){
@@ -49,7 +78,7 @@ public class CreateHost {
 
     private void initViewVideo(){
         vboxContainer.getChildren().clear();
-        File videoFile = new File("src/main/resources/video.mp4");
+        File videoFile = new File(videoFileAdress);
 
         String videoUri = videoFile.toURI().toString();
         Media media = new Media(videoUri);
